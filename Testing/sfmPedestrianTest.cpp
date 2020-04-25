b2321e6e695a3df3426c3714e9bfcd5d7d8e7969
Griffin Bassman <griffinbassman@Griffins-MacBook-Pro.local>
2020-04-25 15:07:10 -0400

    Add basic tests for Pedestrian class. Looks at instantiation as well as boundaries

---
+++ Testing/sfmPedestrianTest.cpp
@@ -0,0 +1,67 @@
+/*=============================================================================
+
+  PHAS0100ASSIGNMENT2: PHAS0100 Assignment 2 Social Force Model
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "catch.hpp"
+#include "sfmCatchMain.h"
+#include "sfmPedestrian.h"
+#include <iostream>
+#include <vector>
+
+// Helper funtion to compare floats
+bool almost_eq(float a, float b, float epsilon) {
+  return abs(a - b) < epsilon;
+}
+float epsilon = .0001; // Small epsilon to ensure floats are equal
+
+TEST_CASE( "Basic Pedestrian instantiation" ) {
+  pos2d orig = pos2d(1.1,1.2);
+  pos2d dest = pos2d(1.3,1.4);
+  dir2d v = dir2d(1.1,52.2);
+  pos2d pos = pos2d(1,1);
+  float v_des = 1.1;
+  float t_relax = 4.4;
+  Pedestrian p(orig, dest, v, pos, v_des, t_relax);
+  REQUIRE(almost_eq(p.orig.x(), 1.1, epsilon));
+  REQUIRE(almost_eq(p.orig.y(), 1.2, epsilon));
+  REQUIRE(almost_eq(p.v.y(), 52.2, epsilon));
+  REQUIRE(almost_eq(p.t_relax, 4.4, epsilon));
+}
+
+TEST_CASE( "Check boundary conditions work when instantiated outside" ) {
+  pos2d orig = pos2d(51.1,1.2);
+  pos2d dest = pos2d(1.3,1.4);
+  dir2d v = dir2d(1.1,52.2);
+  pos2d pos = pos2d(1,1);
+  float v_des = 1.1;
+  float t_relax = 4.4;
+  Pedestrian p(orig, dest, v, pos, v_des, t_relax);
+  REQUIRE(almost_eq(p.orig.x(), 1.1, epsilon));
+}
+
+TEST_CASE( "Check boundary conditions work when moving outside" ) {
+  pos2d orig = pos2d(1.1,1.2);
+  pos2d dest = pos2d(1.3,1.4);
+  dir2d v = dir2d(10,10);
+  pos2d pos = pos2d(45,45);
+  float v_des = 1.1;
+  float t_relax = 4.4;
+  Pedestrian p(orig, dest, v, pos, v_des, t_relax);
+  REQUIRE(almost_eq(p.pos.x(), 45, epsilon));
+  p.pos = p.pos + p.v;
+  REQUIRE(almost_eq(p.pos.x(), 5, epsilon));
+}
+
+
+
+
